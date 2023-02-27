e2cc8f5ae479f08aacb80a91167451c5c541f8da
Rumen Tsolov <r.tsolov86@gmail.com>
2023-02-27 17:38:50 +0200

    v4

--- CPP/OOP/Zadachy-23/1-2-Distance/Distance.cpp
+++ CPP/OOP/Zadachy-23/1-2-Distance/Distance.cpp
@@ -1,20 +1,41 @@
-/*
-Write a program to calculate the (Euclidean) distance between two points p1 {x1, y1} and p2 {x2, y2}. You should write
-a class to represent such points and a method in it that calculates the distance from the point to another point.
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <iomanip> 
 
-Input :
-3 4
-6 8
+class Points{
+    public:
+    double EuclideanDistance;
+    Points(std::istream& iStr);
+    void calculateEuclideanDistance(int &x1, int &x2, int &y1, int &y2);
+    void print();
 
-5.000
+};
 
-3 4
-5 4
+Points::Points(std::istream& iStr){
+    int x1, x2, y1, y2;
+    std::string line;
+    getline(iStr, line);
+    std::istringstream iss1(line);
+    iss1 >> x1 >> y1;
+    getline(iStr, line);
+    std::istringstream iss2(line);
+    iss2 >> x2 >> y2;
+    calculateEuclideanDistance(x1, x2, y1, y2);
+}
 
-2.000
+void Points::calculateEuclideanDistance(int &x1, int &x2, int &y1, int &y2){
+    EuclideanDistance = sqrt(pow((x2 - x1),2) + pow((y2 - y1),2));
+    print();
+};
 
-8 -2
--1 5
+void Points::print(){
+    std::cout << std::fixed << std::setprecision(3) <<EuclideanDistance << std::endl;
+}
 
-11.402
-*/
+int main(){
+
+Points getPoints(std::cin);
+
+return 0;
+}
